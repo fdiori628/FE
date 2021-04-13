449db2c31c66b0fd6407f119f3478246295d3e6a
tester <fdiori628@gmail.com>
2021-04-13 14:25:11 +0800

    update the axios as main func to request the server

--- src/Components/gitHubSearch/Search/index.jsx
+++ src/Components/gitHubSearch/Search/index.jsx
@@ -7,39 +7,37 @@ class Search extends Component {
 
     Search = async () => {
         const searchPara = this.input1.value
-
-
-        // Axios ==========================================================================
-        // async function getData(para){
-        //     try {
-        //         let data = await axios({
-        //             method:'GET',
-        //             url:"http://localhost:3001/" + para
-        //         })
-
-        //         let res = data.data;
-
-        //         // console.log(res)
-
-        //         PubSub.publish('personInfo', res)
-        //     }catch(err){
-
-        //     }
-        // }
-
         
-        // fetch ===========================================================================
+        // Axios ==========================================================================
         async function getData(para) {
             try {
-                let url = 'http://localhost:3001/' + para
-                const respones = await fetch(url);
-                const data = await respones.json();
-                PubSub.publish('personInfo', data)
-            } catch (err) {
-                console.log(err)
-            }
+                let data = await axios({
+                    method: 'GET',
+                    url: "http://localhost:3001/" + para
+                })
 
+                let res = data.data;
+
+                PubSub.publish('personInfo', res)
+
+            } catch (err) {
+
+            }
         }
+
+
+        // fetch ===========================================================================
+        // async function getData(para) {
+        //     try {
+        //         let url = 'http://localhost:3001/' + para
+        //         const respones = await fetch(url);
+        //         const data = await respones.json();
+        //         PubSub.publish('personInfo', data)
+        //     } catch (err) {
+        //         console.log(err)
+        //     }
+
+        // }
 
         getData(searchPara)
     }
