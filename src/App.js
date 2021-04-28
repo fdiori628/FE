38e01b6c8090985e133d9230ec03064a97989462
fdiori <fdiori@vip.qq.com>
2021-04-28 23:19:03 +0800

    update router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
+import { Link, Route } from 'react-router-dom'
 import NewToDoList from './Components/newToDoList/'
+import ToDoList from './Components/ToDoList'
 
 class App extends React.Component {
 
@@ -8,7 +10,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <NewToDoList />
+        <div>
+          <Link className="list-item" to="/newToDoList">New list</Link><br />
+          <Link className="list-item" to="/ToDoList">todo list</Link>
+        </div>
+        <div>
+          <Route path="/newToDoList" component={NewToDoList} />
+          <Route path="/ToDoList" component={ToDoList} />
+        </div>
       </div>
     )
   }
