e9ad11c2c38ddfb47838e54654ac7d78852d534f
fdiori628 <fdiori628@gmail.com>
2021-05-13 18:01:00 +0800

    update check box

--- src/Components/newToDoList/AppFooter/index.jsx
+++ src/Components/newToDoList/AppFooter/index.jsx
@@ -7,7 +7,8 @@ export default class AppFooter extends Component {
 
     state = {
         selectedVaule: 0,
-        totalTodo: 0
+        totalTodo: 0,
+        tempTodos:[]
     }
 
     UNSAFE_componentWillMount() {
@@ -35,14 +36,43 @@ export default class AppFooter extends Component {
         PubSub.unsubscribe(this.token)
     }
 
+    handleCheckAll = ()=>{
+
+        let statusNumber = 0
+
+        this.token2 = PubSub.subscribe('todos', (_, data)=>{
+            this.setState({
+                tempTodos:data
+            })
+        })
+
+        let {tempTodos} = this.state;
+        tempTodos.map((todo)=>{
+            if (todo.done === true) {
+                statusNumber = statusNumber + 1;
+            }
+            return ''
+        })
+
+        console.log(statusNumber)
+        console.log(tempTodos.length)
+        if(statusNumber === 0) {
+            return false
+        }else if(statusNumber === tempTodos.length) {
+            return true
+        }else {
+            return false
+        }
+
+    }
+
 
     render() {
 
         return (
             <div className="appfooter">
-                <input type="checkbox" />
+                <input type="checkbox" defaultChecked = {this.handleCheckAll()}/>
                 <span>{this.state.selectedVaule} / {this.state.totalTodo}</span>
-                {/* <span>1 / 10</span> */}
             </div>
         )
     }
