59046027f180274f00c32ff44ae1aa72f35d78dd
fdiori <fdiori628@gmail.com>
2021-06-10 14:22:17 +0800

    update new version for sql book

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Link, Route } from 'react-router-dom'
-import NewToDoList from './Components/newToDoList/'
-import ToDoList from './Components/ToDoList'
+
 
 class App extends React.Component {
 
@@ -10,14 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <Link className="list-item" to="/newToDoList">New list</Link><br />
-          <Link className="list-item" to="/ToDoList">todo list</Link>
-        </div>
-        <div>
-          <Route path="/newToDoList" component={NewToDoList} />
-          <Route path="/ToDoList" component={ToDoList} />
-        </div>
+          this is app.......
       </div>
     )
   }
