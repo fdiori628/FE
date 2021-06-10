a4728e252cc0e677587351e5d9278991809afe61
fdiori <fdiori628@gmail.com>
2021-06-10 18:21:26 +0800

    update sql home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Link, Route } from 'react-router-dom'
+import SQLHOME from './Components/SQLBOOK/HOME';
 
 
 class App extends React.Component {
@@ -9,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-          this is app.......
+          <SQLHOME />
       </div>
     )
   }
---
+++ src/Components/SQLBOOK/HOME/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./index.css"
+export default class SQLHOME extends Component {
+    render() {
+        return (
+            <div className="sqlHome">
+                this is SQLHOME
+            </div>
+        )
+    }
+}
