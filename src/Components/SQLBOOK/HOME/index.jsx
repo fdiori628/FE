405c09dbfbb394afa0499155e8d5d7da962f74b2
fdiori628 <73645700+fdiori628@users.noreply.github.com>
2021-06-14 15:21:02 +0800

    update sql-home

--- src/Components/SQLBOOK/HOME/index.jsx
+++ src/Components/SQLBOOK/HOME/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import SQLNEV from '../NEV'
 import "./index.css"
 export default class SQLHOME extends Component {
     render() {
         return (
             <div className="sqlHome">
-                this is SQLHOME
+                <SQLNEV />
+                <div className="sql-body">body</div>
             </div>
         )
     }
---
+++ src/Components/SQLBOOK/NEV/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Button} from "antd"
+import "./index.css"
+
+
+export default class SQLNEV extends Component {
+    render() {
+        return (
+            <div className="sql-nev">
+                <Button type="primary">Button</Button>
+            </div>
+        )
+    }
+}
